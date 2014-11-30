69902258f4d75bba78a1337b3d92756ea7c72d4d
filippop1 <sheep3395@gmail.com>
2014-11-30 01:14:34 +0100

    Initial commit

---
+++ src/pl/filippop1/jebacnonpremium/JebacNonPremium.java
@@ -0,0 +1,34 @@
+package pl.filippop1.jebacnonpremium;
+
+import java.util.logging.Level;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JebacNonPremium extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		this.getLogger().log(Level.INFO, "#################");
+		this.getLogger().log(Level.INFO, "# ANTY STEVE ON #");
+		this.getLogger().log(Level.INFO, "#################");
+		if (!Bukkit.getOnlineMode()) {
+			for (int i = 0; i < 20; i++) {
+				this.getLogger().log(Level.INFO, "");
+				this.getLogger().log(Level.INFO, "#########################################");
+				this.getLogger().log(Level.INFO, "#             NON PREMIUM!?             #");
+				this.getLogger().log(Level.INFO, "#########################################");
+				this.getLogger().log(Level.INFO, "#            WYLACZAM SERWER            #");
+				this.getLogger().log(Level.INFO, "#             Buy Minecraft:            #");
+				this.getLogger().log(Level.INFO, "# https://minecraft.net/store/minecraft #");
+				this.getLogger().log(Level.INFO, "#########################################");
+			}
+			this.getServer().shutdown();
+		}
+	}
+	
+	@Override
+	public void onDisable() {
+		this.getLogger().log(Level.INFO, "##################");
+		this.getLogger().log(Level.INFO, "# ANTY STEVE OFF #");
+		this.getLogger().log(Level.INFO, "##################");
+	}
+}
